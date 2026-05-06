9a99fef582b3760a505770b5f1e6557809a8f796
mustafaabdulkhailq49@gmail.com <mustafaabdulkhailq49@gmail.com>
2026-05-07 00:46:44 +0300

    Refactor InsightsView and InsightsViewBody to enhance state management and UI components

--- lib/features/insights/presentation/views/insights_view.dart
+++ lib/features/insights/presentation/views/insights_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:memo/core/app_colors.dart';
 import 'package:memo/features/insights/presentation/widgets/insights_view_body.dart';
 
 class InsightsView extends StatelessWidget {
@@ -7,6 +8,11 @@ class InsightsView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+           backgroundColor:  AppColors.creamWhite,
+      appBar: AppBar(
+        backgroundColor: AppColors.creamWhite,
+        title: const Text('Insights'),
+      ),
       body: SafeArea(child: 
       InsightsViewBody()
       ),
